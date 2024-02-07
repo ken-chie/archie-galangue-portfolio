4d01d7e45fde25d51185da9c0a3940934a239ee7
chie-ai <bacuetesarchie@gmail.com>
2024-02-07 14:45:28 +0800

    Improvements on footer component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="px-6 md:px-7 lg:px-6 w-full pt-24 pb-10">
+  <div class="px-6 md:px-7 lg:px-6 w-full pt-8 lg:pt-24 pb-10">
     <div class="mx-auto max-w-screen-lg">
       <div class="grid grid-cols-12 gap-6 mx-auto max-w-screen-lg">
-        <div class="col-span-7 lg:col-span-3 bg-gradient-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent">
-          Feel free to contact me
+        <div class="col-span-7 lg:col-span-3">
+          <span class="bg-gradient-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent">Feel free to contact me</span>
         </div>
         <div class="col-span-7 lg:col-span-3 text-white order-3 lg:order-2">
           bacuetesarchie@gmail.com
         </div>
         <div class="col-span-5 lg:col-span-3 text-white order-4 lg:order-2">
-          Telg @archie
+          Tel<span class="hidden md:inline-block">e</span>g<span class="hidden md:inline-block">ram</span> <a href="https://t.me/eamon_8" target="_blank">@archie</a>
         </div>
         <div class="col-span-5 lg:col-span-3 flex gap-3">
-          <a href="https://www.facebook.com/8.chiee" target="_blank" class="text-2xl text-white hover:text-white transition">
+          <!-- <a href="https://www.facebook.com/8.chiee" target="_blank" class="text-2xl text-white hover:text-white transition">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 512 512">
             <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
           </a>
@@ -23,9 +23,24 @@
           <a href="https://github.com/chie-ai" target="_blank" class="text-2xl text-white hover:text-white transition">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 496 512">
             <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
-          </a>
+          </a> -->
+          <ClientOnly>
+            <template v-for="(social, index) in socialLinks" :key="index">
+              <a :href="social.link" target="_blank" class="text-2xl text-gray-200 hover:text-white transition">
+                <font-awesome-icon :icon="['fab', social.icon]" />
+              </a>
+            </template>
+          </ClientOnly>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const socialLinks = ref([
+  { link: 'https://www.facebook.com/8.chiee', icon: 'facebook' },
+  { link: 'https://www.linkedin.com/in/archie-galangue-81ba89141/', icon: 'linkedin' },
+  { link: 'https://github.com/chie-ai', icon: 'github' }
+])
+</script>
