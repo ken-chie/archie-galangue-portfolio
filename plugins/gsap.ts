334f0e6a6b5efddba179d2827281fdca0b0d7a6d
chie-ai <bacuetesarchie@gmail.com>
2024-02-07 16:27:37 +0800

    Modified setup in gsap library

--- plugins/gsap.ts
+++ plugins/gsap.ts
@@ -1,20 +1,20 @@
 
-// import { gsap } from "gsap"
-// import { ScrollTrigger } from "gsap/ScrollTrigger"
-// import { ScrollToPlugin } from "gsap/ScrollToPlugin"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { ScrollToPlugin } from "gsap/ScrollToPlugin"
 
-import { gsap } from "gsap-trial"
-import { ScrollTrigger } from "gsap-trial/ScrollTrigger"
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother"
-import { ScrollToPlugin } from "gsap-trial/ScrollToPlugin"
+// import { gsap } from "gsap-trial"
+// import { ScrollTrigger } from "gsap-trial/ScrollTrigger"
+// import { ScrollSmoother } from "gsap-trial/ScrollSmoother"
+// import { ScrollToPlugin } from "gsap-trial/ScrollToPlugin"
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.client) {
     // Enable when deployed in the production
-    // gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
     // Temporarily enabled as '$ScrollSmoother' needs subscription when deployed in the production
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin)
+    // gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin)
   }
 
   return {
