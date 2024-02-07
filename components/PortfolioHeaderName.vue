769d28bf5cf628e4f771326d95a3039bf566f176
chie-ai <bacuetesarchie@gmail.com>
2024-02-07 15:51:47 +0800

    Created component for the porfolio name

---
+++ components/PortfolioHeaderName.vue
@@ -0,0 +1,8 @@
+<template>
+  <span class="self-center text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent">
+    A.G. portfolio
+  </span>
+</template>
+
+<script setup lang="ts">
+</script>
