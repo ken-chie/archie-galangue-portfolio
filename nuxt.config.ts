ad617a8649d70250737ba0c4d1c904148a8e2085
chie-ai <bacuetesarchie@gmail.com>
2024-02-07 15:43:44 +0800

    Changed link icon - Added runtime config variable - Inclusion of fontawesone css styles - Configured typescript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,19 +8,28 @@ export default defineNuxtConfig({
       title: 'Portfolio - Archie Galangue',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'icon', type: 'image/png', href: "/favicon.ico" }]
     }
   },
   runtimeConfig: {
+    apiBase: process.env.BASE_URL,
     public: {
-      apiBase: ''
+      apiBase: process.env.BASE_URL
     }
   },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ],
   css: [
     '@/assets/css/main.scss',
-    '@/assets/css/tailwind.css'
+    '@/assets/css/tailwind.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+    
   ],
-  plugins: [
-    '~/plugins/gsap.js'
-  ]
+  plugins: [],
+  typescript: {
+    typeCheck: true,
+    strict: true
+  }
 })
