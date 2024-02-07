d96726d671f451215b4d40b9aec434fa37d817f2
chie-ai <bacuetesarchie@gmail.com>
2024-02-07 14:57:20 +0800

    Improvements on header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,56 +1,35 @@
 <template>
-  <header id="introduction">
+  <header>
     <nav class="px-5 md:px-7 lg:px-6 py-5 opacity-0 scroll-trigger-header" :class="{ 'opacity-100': header }">
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-lg py-1 md:py-2">
-        <a href="#introduction" class="flex lg:basis-1/3 md:basis-1/4 sm:basis-full shrink items-center w-auto">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap bg-gradient-to-r from-teal-400 to-yellow-200 bg-clip-text text-transparent">A.G. portfolio</span>
+        <a href="#" class="flex lg:basis-1/3 md:basis-1/4 sm:basis-full shrink items-center w-auto">
+          <portfolio-header-name />
         </a>
         <div class="flex lg:basis-1/3 md:basis-1/4 sm:basis-full shrink gap-5 md:gap-3 md:order-2 w-auto justify-end">
-          <a href="https://www.facebook.com/8.chiee" target="_blank" class="text-2xl text-white hover:text-white transition">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 512 512">
-            <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
-          </a>
-          <a href="https://www.linkedin.com/in/archie-galangue-81ba89141/" target="_blank" class="text-2xl text-white hover:text-white transition">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 448 512">
-            <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
-          </a>
-          <a href="https://github.com/chie-ai" target="_blank" class="text-2xl text-white hover:text-white transition">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 496 512">
-            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
-          </a>
+          <ClientOnly>
+            <template v-for="(social, index) in socialLinks" :key="index">
+              <a :href="social.link" target="_blank" class="text-2xl text-gray-200 hover:text-white transition">
+                <!-- <font-awesome-layers> -->
+                <font-awesome-icon :icon="['fab', social.icon]" />
+                <!-- </font-awesome-layers> -->
+              </a>
+            </template>
+          </ClientOnly>
         </div>
         <div class="flex lg:basis-1/3 md:basis-1/2 sm:basis-full justify-between items-center shrink w-full md:w-auto md:order-1 pt-0 lg:px-0 md:px-4 sm:md:px-7">
           <ul class="flex mt-4 font-normal justify-between flex-row space-x-8 md:mt-0 w-full">
-            <li>
-              <a
-                href="#"
-                class="group text-white py-3 relative w-full"
-                @click="smoothScrollTo('projects')"
-              >
-                Projects
-                <span class="ease absolute bottom-0 h-0.5 block w-0 bg-gradient-to-r group-hover:from-teal-400 group-hover:to-yellow-200 transition-all duration-200 group-hover:w-full"></span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="group text-white py-3 relative w-full"
-                @click="smoothScrollTo('technology')"
-              >
-                Technology
-                <span class="ease absolute bottom-0 h-0.5 block w-0 bg-gradient-to-r group-hover:from-teal-400 group-hover:to-yellow-200 transition-all duration-200 group-hover:w-full"></span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="group text-white py-3 relative w-full"
-                @click="smoothScrollTo('work-experience')"
-              >
-                Work Experience
-                <span class="ease absolute bottom-0 h-0.5 block w-0 bg-gradient-to-r group-hover:from-teal-400 group-hover:to-yellow-200 transition-all duration-200 group-hover:w-full"></span>
-              </a>
-            </li>
+            <template v-for="(nav, index) in navLinks" :key="index">
+              <li>
+                <a
+                  href="#"
+                  class="group text-gray-200 hover:text-white py-3 relative w-full transition"
+                  @click="smoothScrollTo(nav.target)"
+                >
+                  {{ nav.link }}
+                  <span class="ease absolute bottom-0 h-0.5 block w-0 bg-gradient-to-r group-hover:from-teal-400 group-hover:to-yellow-200 transition-all duration-200 group-hover:w-full"></span>
+                </a>
+              </li>
+            </template>
           </ul>
         </div>
       </div>
@@ -58,21 +37,33 @@
   </header>
 </template>
 
-<script setup>
-import { onMounted, ref } from 'vue'
-const { $gsap, $ScrollTrigger } = useNuxtApp()
-// const { $gsap, $ScrollTrigger, $ScrollSmoother } = useNuxtApp()
+<script setup lang="ts">
+const { $gsap } = useNuxtApp()
 
 const header = ref(false)
-const smooth = ref(null)
 
-onMounted(() => {
+const socialLinks = ref([
+  { link: 'https://www.facebook.com/8.chiee', icon: 'facebook' },
+  { link: 'https://www.linkedin.com/in/archie-galangue-81ba89141/', icon: 'linkedin' },
+  { link: 'https://github.com/chie-ai', icon: 'github' }
+])
+
+const navLinks = ref([
+  { link: 'Projects', target: 'projects' },
+  { link: 'Technology', target: 'technology' },
+  { link: 'Work Experience', target: 'work-experience' }
+])
+
+onBeforeMount(() => {
   header.value = true
-  // smooth.value = $ScrollSmoother.create({ smooth: 1, effects: true, smoothTouch: 0.1, })
-  $gsap.fromTo('.scroll-trigger-header', { y: -100, opacity: 0 }, { y: 0, duration: 0.7, opacity: 1 })
 })
 
-const smoothScrollTo = (target) => {
+onMounted(() => {
+  const lt = $gsap.timeline()
+  lt.fromTo('.scroll-trigger-header', { y: -50, opacity: 0 }, { y: 0, duration: 1.1, opacity: 1 })
+})
+
+const smoothScrollTo = (target: string) => {
   $gsap.to(window, {
     duration: 1,
     scrollTo: `#${target}`,
